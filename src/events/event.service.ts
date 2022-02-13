39e0acf6f1c67831555978bbb3923bad61528c75
Tôma <thomas.haine@nomadeducation.fr>
2022-02-13 19:47:24 +0100

    remove useless private events in event service

--- src/events/event.service.ts
+++ src/events/event.service.ts
@@ -4,7 +4,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 @Injectable()
 export class EventsService {
-  private events: Event[] = [];
   constructor(
     @InjectModel('Event') private readonly eventModel: Model<Event>,
   ) {}
